cd45adfafe9856053c1cda8a74b47840f1e15af2
Sarah Unruh <SAUnruh@users.noreply.github.com>
2015-09-08 13:07:39 -0500

    Create run_trinity.sh

---
+++ run_trinity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -J Assemble
+#SBATCH -e err.%J
+#SBATCH -o out.%J
+#SBATCH --mem-per-cpu=4G
+#SBATCH -n 10
+#SBATCH -t 4000
+
+#For fungi, use jaccard_clip option to remove fusion transcripts
+#Need to tweak memory requirements for these jobs, they will change with size of files
+#$1 and $2 are input files $3 is sp-name that will create sp-name_trinity_out directory for output
+
+#ulimit -s unlimited
+perl /data/pireslab/Asparagales/bin/trinityrnaseq_r20140717/Trinity --seqType fq --JM 40G --left $1 --right $2 --output ./$3_trinity_out --CPU 10 --jaccard_clip
